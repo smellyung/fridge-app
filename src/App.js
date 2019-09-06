dadba5cbbb507662105c535bc300d3d9579827c7
farajsharman <farajsharman@yahoo.com>
2019-09-06 09:50:45 +0100

    add item list structure and expiry dates

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let items = [products];
+  let sorted = items.sort((a,b) => a.expireydate -b.expireydate);
   return (
     <div className="App">
       <h1>Fridge App!!</h1>
+      <ul className='listItem'>{  
+        sorted.map(item => {
+          return (
+            <div>
+              <li> {ItemName} </li>
+              <button onClick={()=> deleteItem}>X</button>
+            </div>
+          );
+        })
+      }</ul> 
     </div>
   );
 }
 
-let items = [products]
-let sorted = items.sort ((a,b) => a.expireydate -b.expireydate>
-<ul>  
-return sorted.map (item =>) {
-  return <ul className = 'listItem'> </ul>
-    <li> {ItemName} </li>
-    <button onClick={()=> deleteItem}>X</button>
-</ul>
-  }
 export default App;
