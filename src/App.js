7d9765f1111f5504692a1abf86d2bd54cfb7492b
farajsharman <farajsharman@yahoo.com>
2019-09-12 12:18:18 +0100

    fixed deleteItem method to delete unique items

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import products from './ItemsList';
 
 function App() {
   const [itemList, setItemList] = useState(products);
-  function deleteItem(remove) {
-    setItemList(itemList.filter(item => item.name !== remove));
+  function deleteDate(date) {
+    setItemList(itemList.filter(item => item.date !== date));
   }
   function deleteAll() {
     setItemList([]);
@@ -45,7 +45,7 @@ function App() {
           return (
             <div className='ItemList'>
               <li key={item.name} id={item.name}> {item.name} {item.date.getDate()}/{item.date.getMonth() + 1}/{item.date.getFullYear()}</li>
-              <button onClick={() => deleteItem(item.name)} className='Button'><img src={bin} className='Bin-logo' alt='bin logo' /></button>
+              <button onClick={() => deleteDate(item.date)} className='Button'><img src={bin} className='Bin-logo' alt='bin logo' /></button>
             </div>
           );
         })
