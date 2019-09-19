391e644d575dfe7ec6ba091390aa0f584a5ee46c
mitsurubil <mitsurubil@hotmail.com>
2019-09-19 15:17:00 +0100

    Deploy app

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const db = require('./db');
+const path = require('path');
 
 app.use(express.urlencoded())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "../../build")));
 
 db.connect().then(dbo => {
     app.get('/rest/products', (req, res) => {
@@ -14,5 +16,9 @@ db.connect().then(dbo => {
         })
     });
  })
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "../../build", 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
