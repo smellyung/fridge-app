bed9b4f61141f089228717b2ca90cf287986617c
smellyung <kelly.ung@centrica.com>
2019-09-25 11:08:59 +0100

    implement google login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../src/components/Logo';
 import Form from '../src/components/Form';
+import Login from '../src/components/LoginFormTemp';
 import '../src/App.css';
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  // useEffect(() => {
+  //   window.gapi.signin2.render('g-signin2', {
+  //     'scope': 'https://www.googleapis.com/auth/plus.login',
+  //     'width': 200,
+  //     'height': 50,
+  //     'longtitle': true,
+  //     'theme': 'dark',
+  //     'onsuccess': onSignIn
+  //   })
+  // })
   return (
     <div className='App'>
+      <Login />
+      
       <Logo />
       <Form />
     </div>
---
+++ src/components/LoginFormTemp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+const responseGoogle = (response) => {
+    console.log(response);
+}
+
+export default function Login() {
+    return (
+        <div className='loginForm'>
+            <input className='firstName' />
+            <input className='lastName' />
+            <div id='googleButton'>
+                <GoogleLogin
+                    clientId="686578234039-g8l4uljoh00uf88e87kviq9qigrltoit.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+            </div>
+        </div>
+    );
+}
